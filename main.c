39c250f06dd325c4344f39ac819888420e203d47
nzander2019 <61376760+nzander2019@users.noreply.github.com>
2020-02-23 02:09:16 -0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+/*
+Programmers: Aida Must and Nora Zander
+Date: February 22, 2020
+Program: MINDSPACE
+Description: This is a text-based escape room game.
+*/
+
+# include "header.h"
+
+/*
+Function: main
+Date Created: February 22, 2020
+Date Last Modified: February 23, 2020
+Description: runs main program
+Parameters: none
+Returns: 0
+*/
+int main()
+{
+	system("color 0B");
+	menu();
+	return 0;
+}
